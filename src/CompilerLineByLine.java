d291a30af5901de4adfb49ef4e1d0b1718c122dd
king <etangoking6@gmail.com>
2023-04-04 17:33:10 +0200

    Definition

---
+++ src/CompilerLineByLine.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class CompilerLineByLine {
+    public static void main(String[] args) {
+
+        /*The variables num, numm, nummm, summation, and multi are not declared as valid data types. In addition, the keyword INTEG is not a valid keyword in any programming language I am aware of.
+
+The assignment operator =-/ is not a valid operator in any programming language. The correct operator for subtraction is -.
+
+The expression <s%*h - j / w - d +*$& is not a valid expression and does not make any sense.
+
+The input statement INPUT num, numm, nummm should come before the calculation of numm.
+
+The variable temp is not declared and assigned a value before it is used in the expression WRITEE temp.
+
+The expression J/K-p+w*Q/s contains variables that are not declared or assigned values.
+
+To fix these issues, you would need to revise the program and use a valid programming language with proper syntax. Here is an example of how you could write a program in Python to accomplish the same task of finding the summation of three numbers and calculating and printing out temp:*/
+
+    }
+}
